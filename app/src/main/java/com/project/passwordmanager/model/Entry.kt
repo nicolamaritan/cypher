90e4e4fe864923f7ce6ff650d0462424831f4111
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-08 17:14:53 +0200

    Added documentation and updated Entry.kt

--- app/src/main/java/com/project/passwordmanager/model/Entry.kt
+++ app/src/main/java/com/project/passwordmanager/model/Entry.kt
@@ -1,3 +1,3 @@
 package com.project.passwordmanager.model
 
-data class Entry(val service: String, val password: String)
+data class Entry(val service: String, val password: String, val encrypted: Boolean = true)
--- app/src/main/java/com/project/passwordmanager/model/WidgetData.kt
+++ app/src/main/java/com/project/passwordmanager/model/WidgetData.kt
@@ -4,41 +4,97 @@ class WidgetData private constructor()
 {
     companion object
     {
+        /**
+         * Checks if widget data with the given ID exists.
+         *
+         * @param appWidgetId The ID of the widget to check.
+         * @return true if widget data with the given ID exists, false otherwise.
+         */
         fun hasWidgetData(appWidgetId: Int) =
             widgetDataMap.containsKey(appWidgetId)
 
+
+        /**
+         * Creates widget data with the given ID.
+         *
+         * @param appWidgetId The ID of the widget to create data for.
+         */
         fun createWidgetData(appWidgetId: Int)
         {
             widgetDataMap[appWidgetId] = WidgetData()
         }
 
-        fun deleteWidgetData(appWidgetId: Int) =
+        /**
+         * Deletes widget data with the given ID.
+         *
+         * @param appWidgetId The ID of the widget to delete data for.
+         */
+        fun deleteWidgetData(appWidgetId: Int)
+        {
             widgetDataMap.remove(appWidgetId)
+        }
 
-        fun getWidgetData(appWidgetId: Int) =
+        /**
+         * Gets widget data with the given ID.
+         *
+         * @param appWidgetId The ID of the widget to get data for.
+         * @return The widget data for the given ID.
+         */
+        fun getWidgetData(appWidgetId: Int): WidgetData? =
             widgetDataMap[appWidgetId]
 
         private val widgetDataMap = mutableMapOf<Int, WidgetData>()
         val TAG = WidgetData::class.java
+
     }
 
+    /**
+     * Encrypts the entry with the given ID.
+     *
+     * @param entryId The ID of the entry to encrypt.
+     */
     fun encrypt(entryId: Int)
     {
 
     }
 
+    /**
+     * Decrypts the entry with the given ID.
+     *
+     * @param entryId The ID of the entry to decrypt.
+     */
     fun decrypt(entryId: Int)
     {
 
     }
 
-    fun isEncrypted(entryId: Int)
+    /**
+     * Checks if the entry with the given ID is encrypted.
+     *
+     * @param entryId The ID of the entry to check.
+     * @return true if the entry is encrypted, false otherwise.
+     */
+    fun isEncrypted(entryId: Int) =
+        getEntry(entryId).encrypted
+
+    /**
+     * Gets the entry with the given ID.
+     *
+     * @param entryId The ID of the entry to get.
+     * @return The entry with the given ID.
+     */
+    fun getEntry(entryId: Int) =
+        entries[entryId]
+
+    /**
+     * Removes the entry with the given ID.
+     *
+     * @param entryId The ID of the entry to remove.
+     */
+    fun removeEntry(entryId: Int)
     {
-
+        entries.removeAt(entryId)
     }
-
-    fun getEntry(entryId: Int) = entries[entryId]
-    fun removeEntry(entryId: Int) = entries.removeAt(entryId)
 
 
     private val entries = mutableListOf<Entry>()
