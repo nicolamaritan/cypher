28d36da50b43cc9586749253e02b957c358d3170
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-18 09:32:04 +0200

    Cryptography.kt and Hashing.kt are now object class (https://kotlinlang.org/docs/object-declarations.html#object-declarations-overview)

--- app/src/main/java/com/project/passwordmanager/security/Hashing.kt
+++ app/src/main/java/com/project/passwordmanager/security/Hashing.kt
@@ -5,21 +5,18 @@ import java.security.MessageDigest
 /**
  * A utility class for generating SHA-256 hash values of input strings.
  */
-class Hashing
+object Hashing
 {
-    companion object
+    /**
+     * Computes the SHA-256 hash value of the given input string.
+     * @param input the input string to be hashed
+     * @return the SHA-256 hash value as a hexadecimal string
+     */
+    fun sha256(input: String): String
     {
-        /**
-         * Computes the SHA-256 hash value of the given input string.
-         * @param input the input string to be hashed
-         * @return the SHA-256 hash value as a hexadecimal string
-         */
-        fun sha256(input: String): String
-        {
-            val bytes = input.toByteArray()
-            val md = MessageDigest.getInstance("SHA-256")
-            val digest = md.digest(bytes)
-            return digest.fold("") { str, it -> str + "%02x".format(it) }
-        }
+        val bytes = input.toByteArray()
+        val md = MessageDigest.getInstance("SHA-256")
+        val digest = md.digest(bytes)
+        return digest.fold("") { str, it -> str + "%02x".format(it) }
     }
 }
