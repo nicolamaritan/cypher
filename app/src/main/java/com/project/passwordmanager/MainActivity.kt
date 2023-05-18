0c851e9a09d2d1777338b04614ccc8c3bf7e5685
aless <alessandrocorro01@gmail.com>
2023-05-18 15:47:32 +0200

    Toolbar implementation

--- app/src/main/java/com/project/passwordmanager/MainActivity.kt
+++ app/src/main/java/com/project/passwordmanager/MainActivity.kt
@@ -2,11 +2,14 @@ package com.project.passwordmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
 }
