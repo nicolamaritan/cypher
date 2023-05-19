e5068b04b4302bac4cd7df6061e3f1b7ba392687
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-19 19:06:49 +0200

    Added view binding for CredentialsFragment.kt. Removed tool ignore in activity_main.xml.

--- app/src/main/java/com/project/passwordmanager/fragments/CredentialsFragment.kt
+++ app/src/main/java/com/project/passwordmanager/fragments/CredentialsFragment.kt
@@ -6,15 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.project.passwordmanager.R
+import com.project.passwordmanager.databinding.FragmentCredentialsBinding
 
 class CredentialsFragment : Fragment()
 {
+    private var _binding: FragmentCredentialsBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_credentials, container, false)
+        _binding = FragmentCredentialsBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        // Test binding
+        binding.helloTv.text = "Binding changed the textview"
+
+        return view
+    }
+
+    override fun onDestroyView()
+    {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
