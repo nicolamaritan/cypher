aebfdda1209e706e2ea561d73b97e631084300cd
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-19 19:22:25 +0200

    Added ViewModel class for CredentialsFragment.kt and added dependencies.

--- app/src/main/java/com/project/passwordmanager/fragments/CredentialsFragment.kt
+++ app/src/main/java/com/project/passwordmanager/fragments/CredentialsFragment.kt
@@ -5,19 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.project.passwordmanager.R
 import com.project.passwordmanager.databinding.FragmentCredentialsBinding
+import com.project.passwordmanager.viewmodels.CredentialsViewModel
 
 class CredentialsFragment : Fragment()
 {
     private var _binding: FragmentCredentialsBinding? = null
     private val binding get() = _binding!!
+    lateinit var viewModel: CredentialsViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentCredentialsBinding.inflate(inflater, container, false)
         val view = binding.root
+        viewModel = ViewModelProvider(this)[CredentialsViewModel::class.java]
 
         // Test binding
         binding.helloTv.text = "Binding changed the textview"
