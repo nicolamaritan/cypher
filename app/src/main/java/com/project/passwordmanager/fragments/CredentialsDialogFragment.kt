27f047edde0c3ce4d38b38a9433de38ec4a5a48d
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-23 09:11:13 +0200

    Polished CredentialsDialogFragment.kt and CredentialsViewModel.kt

--- app/src/main/java/com/project/passwordmanager/fragments/CredentialsDialogFragment.kt
+++ app/src/main/java/com/project/passwordmanager/fragments/CredentialsDialogFragment.kt
@@ -2,6 +2,7 @@ package com.project.passwordmanager.fragments
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -11,28 +12,19 @@ import androidx.fragment.app.DialogFragment
 import com.project.passwordmanager.R
 import com.project.passwordmanager.common.Logger
 
-class CredentialsDialogFragment private constructor(): DialogFragment()
+class CredentialsDialogFragment(): DialogFragment()
 {
-    companion object
-    {
-        fun newInstance(title: String): CredentialsDialogFragment
-        {
-            val frag = CredentialsDialogFragment()
-            val args = Bundle()
-            args.putString("title", title)
-            frag.arguments = args
-            return frag
-        }
-
-        private val TAG = CredentialsFragment::javaClass.toString()
-    }
-
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Logger.logCallback(TAG, "onCreateView", "CredentialsDialogFragment")
         return layoutInflater.inflate(R.layout.dialog_credentials, container, false)
+    }
+
+    companion object
+    {
+        private val TAG = CredentialsDialogFragment::javaClass.toString()
     }
 }
--- app/src/main/java/com/project/passwordmanager/viewmodels/CredentialsViewModel.kt
+++ app/src/main/java/com/project/passwordmanager/viewmodels/CredentialsViewModel.kt
@@ -5,14 +5,16 @@ import androidx.lifecycle.ViewModel
 import com.project.passwordmanager.R
 import com.project.passwordmanager.fragments.CredentialsDialogFragment
 
-
 class CredentialsViewModel : ViewModel()
 {
     fun showDialog(fragmentManager: FragmentManager)
     {
-        val newFragment = CredentialsDialogFragment.newInstance(
-            "R.string.alert_dialog_two_buttons_title"
-        )
-        newFragment.show(fragmentManager, "dialog")
+        val newFragment = CredentialsDialogFragment()
+        newFragment.show(fragmentManager, TAG)
+    }
+
+    companion object
+    {
+        private val TAG = CredentialsViewModel::javaClass.toString()
     }
 }
