ce8fd62fc5cf128733ae20401e0ea6dc2b3200c9
Eddi3k <96292426+Eddi3k@users.noreply.github.com>
2023-05-23 23:11:37 +0200

    PwmAdapter added
    added the recyclerView adapter and also the recyclerview in fragment_credentials

---
+++ app/src/main/java/com/project/passwordmanager/recyclerView/PwmAdapter.kt
@@ -0,0 +1,44 @@
+package com.project.passwordmanager.recyclerView
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.project.passwordmanager.R
+
+//NOTE: modify with the database
+class PwmAdapter(private var app_lst: Array<String>, private var user_lst: Array<String>, private var pw_lst: Array<String>):
+    RecyclerView.Adapter<PwmAdapter.PwmViewHolder>(){
+
+
+    class PwmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val appName: TextView = itemView.findViewById(R.id.service)
+        val appUser: TextView = itemView.findViewById(R.id.user)
+        val appPw: TextView = itemView.findViewById(R.id.password)
+
+        fun bind(name:String, user:String, pw:String){
+            appName.text = name
+            appUser.text = user
+            appPw.text = pw
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PwmViewHolder {
+        val view =  LayoutInflater.from(parent.context)
+            .inflate(R.layout.password_manager_item, parent, false)
+
+        return PwmViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return user_lst.size
+    }
+
+    override fun onBindViewHolder(holder: PwmViewHolder, position: Int) {
+        holder.bind(app_lst[position], user_lst[position], pw_lst[position])
+    }
+
+
+}
