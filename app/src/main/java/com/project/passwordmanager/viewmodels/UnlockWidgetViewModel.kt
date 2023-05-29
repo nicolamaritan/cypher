44feccd0646e4b4b0fe33e1a19c8a428eccaa666
Nicola Maritan <nicolamaritan01@gmail.com>
2023-05-29 21:59:35 +0200

    Added documentation for UnlockWidgetViewModel.kt

--- app/src/main/java/com/project/passwordmanager/viewmodels/UnlockWidgetViewModel.kt
+++ app/src/main/java/com/project/passwordmanager/viewmodels/UnlockWidgetViewModel.kt
@@ -5,16 +5,25 @@ import androidx.lifecycle.ViewModel
 import com.project.passwordmanager.model.WidgetData
 import com.project.passwordmanager.security.Hashing
 
-class UnlockWidgetViewModel : ViewModel()
-{
+/**
+ * UnlockWidgetViewModel is a ViewModel class that handles the unlocking logic for the widget.
+ *
+ * @property appWidgetId The ID of the app widget to be unlocked.
+ */
+class UnlockWidgetViewModel : ViewModel() {
+
     var appWidgetId: Int = AppWidgetManager.INVALID_APPWIDGET_ID
 
-    fun unlock(insertedPassword: String) : Boolean
-    {
+    /**
+     * Attempts to unlock the widget by comparing the hashed inserted password with the hashed master password.
+     *
+     * @param insertedPassword The password entered by the user.
+     * @return True if the password is correct and the widget is unlocked, false otherwise.
+     */
+    fun unlock(insertedPassword: String): Boolean {
         val hashedInsertedPassword = Hashing.sha256(insertedPassword)
 
-        if (hashedInsertedPassword == Hashing.sha256("MASTER"))
-        {
+        if (hashedInsertedPassword == Hashing.sha256("MASTER")) {
             WidgetData.getWidgetData(appWidgetId).unlock(insertedPassword)
             return true
         }
