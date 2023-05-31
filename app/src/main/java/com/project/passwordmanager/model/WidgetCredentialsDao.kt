0a482f791506eb7c06f8fa5c5fd9a960e7216445
nicolamaritan <nicolamaritan01@gmail.com>
2023-05-31 17:39:31 +0200

    Added method to dao

--- app/src/main/java/com/project/passwordmanager/model/WidgetCredentialsDao.kt
+++ app/src/main/java/com/project/passwordmanager/model/WidgetCredentialsDao.kt
@@ -14,6 +14,9 @@ interface WidgetCredentialDao {
     @Query("SELECT * FROM widget_credentials_table WHERE credentialId = :credentialId AND appWidgetId = :appWidgetId")
     suspend fun getWidgetCredential(credentialId: Long, appWidgetId: Int): WidgetCredential?
 
+    @Query("SELECT * FROM widget_credentials_table WHERE appWidgetId = :appWidgetId")
+    suspend fun getWidgetCredentialsByAppWidgetId(appWidgetId: Int): List<WidgetCredential>
+
     @Update
     suspend fun update(widgetCredential: WidgetCredential)
 }
