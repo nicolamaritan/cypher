c30908c1798f73ef84ca06335ca8b0bdbb571f11
nicolamaritan <nicolamaritan01@gmail.com>
2023-06-02 22:21:32 +0200

    Updated unlock widget

--- app/src/main/java/com/project/passwordmanager/viewmodels/UnlockWidgetViewModel.kt
+++ app/src/main/java/com/project/passwordmanager/viewmodels/UnlockWidgetViewModel.kt
@@ -1,9 +1,10 @@
 package com.project.passwordmanager.viewmodels
 
 import android.appwidget.AppWidgetManager
-import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.project.passwordmanager.common.Utils
+import com.project.passwordmanager.R
 import com.project.passwordmanager.security.Hashing
 
 /**
@@ -15,14 +16,25 @@ class UnlockWidgetViewModel : ViewModel() {
 
     var appWidgetId: Int = AppWidgetManager.INVALID_APPWIDGET_ID
 
+    private var _toastStringId = MutableLiveData<Int>()
+    val toastStringId: LiveData<Int>
+        get() = _toastStringId
+
     /**
      * Attempts to unlock the widget by comparing the hashed inserted password with the hashed master password.
      *
      * @param insertedPassword The password entered by the user.
      * @return True if the password is correct and the widget is unlocked, false otherwise.
      */
-    fun unlock(context: Context, insertedPassword: String): Boolean {
+    fun unlock(hashedTrueMasterPassword: String, insertedPassword: String): Boolean {
         val hashedInsertedPassword = Hashing.sha256(insertedPassword)
-        return  (hashedInsertedPassword == Utils.getHashedMasterPassword(context))
+        if (hashedInsertedPassword == hashedTrueMasterPassword)
+        {
+            _toastStringId.value = R.string.credential_unlocked_successfully
+            return true
+        }
+
+        _toastStringId.value = R.string.wrong_master_password
+        return false
     }
 }
