4f4e350fbe622569daa75aadd000600a86fd122d
nicolamaritan <nicolamaritan01@gmail.com>
2023-06-03 00:33:26 +0200

    fixed missing resource and import

--- app/src/main/java/com/project/passwordmanager/fragments/ModifyDialogFragment.kt
+++ app/src/main/java/com/project/passwordmanager/fragments/ModifyDialogFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
 import com.project.passwordmanager.common.Logger
@@ -66,7 +67,7 @@ class ModifyDialogFragment(private val credentialId : Long): DialogFragment() {
         viewModel.toastStringId.observe(this) {
             it.let {
                 val toastMessage = requireContext().getString(it)
-                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+                Toast.makeText(context, toastMessage, Toast.LENGTH_LONG).show()
             }
         }
 
