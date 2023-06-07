a54f6356b143f795e8859649dd894f20a14a3e80
nicolamaritan <nicolamaritan01@gmail.com>
2023-06-07 13:10:20 +0200

    Updated documentation

--- app/src/main/java/com/project/passwordmanager/model/CredentialDao.kt
+++ app/src/main/java/com/project/passwordmanager/model/CredentialDao.kt
@@ -43,15 +43,39 @@ interface CredentialDao {
     @Query("SELECT * FROM credentials_table ORDER BY id ASC")
     fun getAll(): LiveData<List<Credential>>
 
+    /**
+     * Retrieves all the credentials from the credentials_table in ascending order by date.
+     * The result is wrapped in a LiveData object, allowing for automatic updates in the UI.
+     *
+     * @return A LiveData object containing a list of all credentials sorted by date.
+     */
     @Query("SELECT * FROM credentials_table ORDER BY date")
     fun getSortedByDate(): LiveData<List<Credential>>
 
+    /**
+     * Retrieves a credential from the credentials_table based on its ID.
+     *
+     * @param credentialId The ID of the credential to retrieve.
+     * @return The retrieved Credential object, or null if not found.
+     */
     @Query("SELECT * FROM credentials_table WHERE id = :credentialId")
     fun getCredentialById(credentialId: Int): Credential?
 
+    /**
+     * Retrieves all the credentials from the credentials_table in ascending order by username.
+     * The result is wrapped in a LiveData object, allowing for automatic updates in the UI.
+     *
+     * @return A LiveData object containing a list of all credentials sorted by username.
+     */
     @Query("SELECT * FROM credentials_table ORDER BY username")
     fun getSortedByUsername(): LiveData<List<Credential>>
 
+    /**
+     * Retrieves all the credentials from the credentials_table in ascending order by service.
+     * The result is wrapped in a LiveData object, allowing for automatic updates in the UI.
+     *
+     * @return A LiveData object containing a list of all credentials sorted by service.
+     */
     @Query("SELECT * FROM credentials_table ORDER BY service")
     fun getSortedByService(): LiveData<List<Credential>>
 }
--- app/src/main/java/com/project/passwordmanager/model/CredentialRepository.kt
+++ app/src/main/java/com/project/passwordmanager/model/CredentialRepository.kt
@@ -2,9 +2,21 @@ package com.project.passwordmanager.model
 
 import androidx.lifecycle.LiveData
 
+/**
+ * The repository class for managing Credential data. Data accessed from
+ * here is primarily fetched from the widgets.
+ *
+ * @param credentialDao The CredentialDao used for data retrieval and manipulation.
+ */
 class CredentialRepository(credentialDao: CredentialDao)
 {
-
+    /**
+     * LiveData object that holds a list of all credentials.
+     */
     val allCredentials: LiveData<List<Credential>> = credentialDao.getAll()
+
+    /**
+     * LiveData object that holds a list of all credentials sorted by date.
+     */
     val allCredentialSortedByDate: LiveData<List<Credential>> = credentialDao.getSortedByDate()
 }
