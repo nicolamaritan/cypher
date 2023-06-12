48ca54effb0334fc642e56308a9ea18340195948
nicolamaritan <nicolamaritan01@gmail.com>
2023-06-12 22:45:53 +0200

    Updated localization strings, polished ordering.

--- app/src/main/java/com/project/passwordmanager/activities/AddCredentialActivity.kt
+++ app/src/main/java/com/project/passwordmanager/activities/AddCredentialActivity.kt
@@ -2,11 +2,20 @@ package com.project.passwordmanager.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.project.passwordmanager.R
+import com.project.passwordmanager.databinding.ActivityAddCredentialBinding
 
-class AddCredentialActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class AddCredentialActivity : AppCompatActivity()
+{
+    private lateinit var binding: ActivityAddCredentialBinding
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_credential)
+        binding = ActivityAddCredentialBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // Fully replaces the original bar with a toolbar
+        val toolbar = binding.toolbar
+        setSupportActionBar(toolbar)
     }
 }
